f050db0d59393a4ecc5e3b26d69d5df88df166c2
Alexander Hoth <alexander.hoth@me.com>
2019-04-01 15:36:46 +0200

    Disable ssl verification

--- lib/fastlane/plugin/download_file/actions/download_file_action.rb
+++ lib/fastlane/plugin/download_file/actions/download_file_action.rb
@@ -21,7 +21,7 @@ module Fastlane
           progress = 0
           File.open(destination_path, "wb") do |saved_file|
             # the following "open" is provided by open-uri
-            open(params[:url], "rb", http_basic_authentication: http_basic_authentication_enabled ? http_basic_authentication : nil, :content_length_proc => lambda {|t|
+            open(params[:url], "rb", http_basic_authentication: http_basic_authentication_enabled ? http_basic_authentication : nil, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, :content_length_proc => lambda {|t|
               if t && 0 < t
                 step = t / 10
                 partial = step
